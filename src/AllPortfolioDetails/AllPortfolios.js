089fcfad7162fb32b0e250caace5e6bdc3f6ed87
Gregdev9 <johngregdev@gmail.com>
2025-05-08 09:20:41 +0100

    move exoi ai

--- src/AllPortfolioDetails/AllPortfolios.js
+++ src/AllPortfolioDetails/AllPortfolios.js
@@ -1,92 +1,89 @@
-import KhaimeImage from '../assets/khaimi.png'
 import BuilderImage from '../assets/builder.png'
-import SenexPayImage from '../assets/senex_pay.png'
-import PaperLinImage from '../assets/paperlink.png'
-import ExoAiImage from '../assets/exoai.png'
-import DXBImage from '../assets/dxb.png'
-import investyieldImage from '../assets/investyield.png'
-import subhaImage from '../assets/subha.png'
 import dunubeImage from '../assets/danube.png'
+import DXBImage from '../assets/dxb.png'
+import ExoAiImage from '../assets/exoai.png'
+import investyieldImage from '../assets/investyield.png'
+import KhaimeImage from '../assets/khaimi.png'
 import newImage from '../assets/newpage.png'
-
+import PaperLinImage from '../assets/paperlink.png'
+import SenexPayImage from '../assets/senex_pay.png'
+import subhaImage from '../assets/subha.png'
 
 export const allPortfolios = [
   {
+    id: 44,
+    src: ExoAiImage,
+    link: 'https://exoai.tech/',
+    name: 'ExoAi',
+    disc: '',
+  },
+
+  {
     id: 1,
     src: KhaimeImage,
-    link: "https://www.khaime.ai/",
-    name: "Khaime",
-    disc: "",
+    link: 'https://www.khaime.ai/',
+    name: 'Khaime',
+    disc: '',
   },
   {
     id: 7,
     src: BuilderImage,
-    link: "https://www.builder.ai/",
-    name: "Builder Ai",
-    disc: "",
+    link: 'https://www.builder.ai/',
+    name: 'Builder Ai',
+    disc: '',
   },
-  
+
   {
     id: 2,
     src: SenexPayImage,
-    link: "https://senexpay.com/",
-    appLink: "https://apps.apple.com/us/app/senexpay-buy-store-bitcoin/id1627864145",
-    name: "SenexPay",
-    disc: "",
+    link: 'https://senexpay.com/',
+    appLink: 'https://apps.apple.com/us/app/senexpay-buy-store-bitcoin/id1627864145',
+    name: 'SenexPay',
+    disc: '',
   },
-  {
-    id: 44,
-    src: ExoAiImage,
-    link: "https://exoai.tech/",
-    name: "ExoAi",
-    disc: "",
-  },
- 
-    
+
   {
     id: 3,
     src: DXBImage,
-    link: "https://www.dxbstart.com/",
-    name: "Dxbstart",
-    disc: "",
+    link: 'https://www.dxbstart.com/',
+    name: 'Dxbstart',
+    disc: '',
   },
   {
     id: 9,
     src: investyieldImage,
-    link: "https://play.google.com/store/apps/details?id=com.investyield&hl=en",
-    name: "Turbomoni",
-    disc: "",
+    link: 'https://play.google.com/store/apps/details?id=com.investyield&hl=en',
+    name: 'Turbomoni',
+    disc: '',
   },
   {
     id: 4,
     src: subhaImage,
-    link: "https://www.sobhapromotions.com/",
-    name: "Sobha Promotions",
-    disc: "",
+    link: 'https://www.sobhapromotions.com/',
+    name: 'Sobha Promotions',
+    disc: '',
   },
   {
     id: 5,
     src: dunubeImage,
-    link: "https://danubeproperties.com/",
-    name: "Danube Properties",
-    disc: "",
+    link: 'https://danubeproperties.com/',
+    name: 'Danube Properties',
+    disc: '',
   },
-  
+
   {
     id: 6,
     src: newImage,
-    link: "https://www.dubai-townhouse.com/",
-    name: "Dubai Townhouse",
-    disc: "",
+    link: 'https://www.dubai-townhouse.com/',
+    name: 'Dubai Townhouse',
+    disc: '',
   },
 
   {
     id: 8,
     src: PaperLinImage,
-    link: "https://paperlink.app/",
-    name: "Paperlink",
-    disc: "",
+    link: 'https://paperlink.app/',
+    name: 'Paperlink',
+    disc: '',
   },
- 
-  
-];
+]
